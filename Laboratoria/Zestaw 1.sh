a5d8613c2060a055c1d40c6c9f4be675cba0d32a
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-16 19:34:19 +0100

    Create

---
+++ Laboratoria/Zestaw 1.sh
@@ -0,0 +1,12 @@
+Zadanie 1
+awk '$5 < 30 {print}' dane.txt | grep -e "^[A]" -e "^[Z]"
+
+Zadanie 2
+awk '$3 == "Warszawa" {print}' dane.txt
+grep "Warszawa" dane.txt
+
+Zadanie 3
+awk '$4 >= 4 {print}' dane.txt | cut -f 1-2 | tail -n20 | sort
+
+Zadanie 4
+awk 'BEGIN {print "Lista osob ktorych nazwiska zaczynaja sie na litere K, D lub S, maja wiecej niz 31 lat oraz dostali ocene 5 lub 6"} $5 > 31 && $4 == 5 || $4 == 6 && $2 ~/K/ || $2 ~/D/ || $2 ~/S/ {print}' dane.txt
