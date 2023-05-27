3372d2dd1ab0c7bb13c81c6d06407d2234b97615
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-05-27 13:45:32 +0200

    DONE

--- Laboratoria/Zestaw 03/Zestaw 03.sh
+++ Laboratoria/Zestaw 03/Zestaw 03.sh
@@ -1,27 +1,33 @@
 Zadanie 1
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 2 ]; then {
 	echo "Nieprawidlowa ilosc parametrow";
+}
 fi
 
-if [ -d $1 ]; then
-	if [ -f $2 ]; then
-	file=`cat $2`;
-	else
-	echo "Nieprawidlowy plik";
-	exit 1
+if [ -d $1 ]; then {
+	if [ -f $2 ]; then {
+		file=`cat $2`;
+	} else {
+		echo "Nieprawidlowy plik";
+		exit 1
+	}
 	fi
-else
-echo "$1 nie jest katalogiem";
+} else {
+	echo "$1 nie jest katalogiem";
+}
 fi
 
-for i in $file
-	do
-	if [ ! -e $i ]; then
-	touch $1/$i ;
-	fi
-done
+for i in $file {
+	do {
+		if [ ! -e $i ]; then {
+			touch $1/$i ;
+		}
+		fi
+	}
+	done
+}
 
 Zadanie 2
 #!/bin/bash
@@ -35,15 +41,19 @@ cd $1
 
 executable_files_counter=0
 
-if A $# -eq 1; then
-	for file in *
-	do
-		if A -x $file; then
-			let executable_files_counter=$executable_files_counter+1;
-		fi
-	done
-else
+if A $# -eq 1; then {
+	for file in * {
+		do {
+			if A -x $file; then {
+				let executable_files_counter=$executable_files_counter+1;
+			}
+			fi
+		}
+		done
+	}
+} else {
 	echo "Bledna ilosc argumentow"
+}
 fi
 
 echo "Liczba plikow z prawem do wykonania: $executable_files_counter"
@@ -61,12 +71,13 @@ read user_input
 
 let random_number=$RANDOM&100
 
-if [ $user_input -eq $random_number ]; then
+if [ $user_input -eq $random_number ]; then {
 	echo "Brawo zgadles liczbe";
-elif [ $user_input -lt $random_number ]; then
+} elif [ $user_input -lt $random_number ]; then {
 	echo "Podana liczba jest mniejsza niz wylosowana";
-elif [ $user_input -gt $random_number ]; then
+} elif [ $user_input -gt $random_number ]; then {
 	echo "Podana liczba jest wieksza niz wylosowana";
+}
 fi
 
 echo "Wylosowana liczba: $random_number"
@@ -76,11 +87,15 @@ Zadanie 6
 
 n=$1
 
-for((i = 1; i <= $n; i++))
-do
-	for((j = 1; j <= i; j++))
-	do
-		echo -n "* ";
-	done
+for((i = 1; i <= $n; i++)) {
+	do {
+		for((j = 1; j <= i; j++)) {
+			do {
+				echo -n "* ";
+			}
+			done
+		}
 	echo
-done
+	done
+	}
+}
